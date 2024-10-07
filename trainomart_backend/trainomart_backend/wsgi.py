e7155c1059e0124e2b426707e135c7ac1ae01ea7
XCode-tech <xcode.tech101@gmail.com>
2024-10-07 17:09:05 +0530

    Update wsgi.py

--- trainomart_backend/trainomart_backend/wsgi.py
+++ trainomart_backend/trainomart_backend/wsgi.py
@@ -7,13 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Set the default settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trainomart_backend.settings')
 
-# Create the WSGI application
-application = get_wsgi_application()
-
-app = application
+app = get_wsgi_application()
