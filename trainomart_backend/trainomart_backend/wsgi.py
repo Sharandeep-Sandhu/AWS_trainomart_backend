1e582c6b5f7af55ccbd37124747a6767d1c32afb
XCode-tech <xcode.tech101@gmail.com>
2024-10-07 13:53:30 +0530

    Update wsgi.py

--- trainomart_backend/trainomart_backend/wsgi.py
+++ trainomart_backend/trainomart_backend/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+# Add the project directory to the Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# Set the default settings module for the 'DJANGO_SETTINGS_MODULE' environment variable
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trainomart_backend.settings')
 
+# Create the WSGI application
 application = get_wsgi_application()
+
+# For Vercel, you need to expose `app` or `handler`
+# So we will define it here
+handler = application
