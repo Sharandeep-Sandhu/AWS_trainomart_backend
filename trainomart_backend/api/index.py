4d8f7360c05bc32ffec511339e5cdc97078defef
XCode-tech <xcode.tech101@gmail.com>
2024-10-07 15:06:36 +0530

    Create index.py

---
+++ trainomart_backend/api/index.py
@@ -0,0 +1,11 @@
+# api/index.py
+
+import os
+from serverless_wsgi import handle_request
+from trainomart_backend.wsgi import application
+
+# Ensure the Django settings module is set
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trainomart_backend.settings')
+
+def handler(request, context):
+    return handle_request(application, request, context)
